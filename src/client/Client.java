be22ef8bd3530dfe4f7cf33b2bc191edeaf2d835
boussnina <boussnina@gmail.com>
2015-12-09 14:57:57 +0100

    imp

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,22 @@
 package client;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.List;
+import java.util.Scanner;
+
+import request.protocols.CreateGameRequest;
+import request.protocols.DeleteGameRequest;
+import request.protocols.GetGameRequest;
+import request.protocols.GetHighScoresRequest;
+import request.protocols.HelloRequest;
+import request.protocols.LoginRequest;
+import request.protocols.LogoutRequest;
+import request.protocols.PlayGameRequest;
+import request.protocols.SetCommandsRequest;
+import dbase.Game;
+
 public class Client {
 
 }
